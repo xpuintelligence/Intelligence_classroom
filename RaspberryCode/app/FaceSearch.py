558bad280ee2c93b9d66822256c5022dd480b981
RoWe98 <wzx8551517@163.com>
2019-04-29 12:04:32 +0800

    Add Readme

--- RaspberryCode/app/FaceSearch.py
+++ RaspberryCode/app/FaceSearch.py
@@ -23,27 +23,28 @@ class FaceSearch:
     def search(self):
 
         for filename in os.listdir(self.facefolder):  # 遍历图片文件夹中所有的人像
-            face_path = self.facefolder + filename;   
-            image = base64.b64encode(open(face_path, 'rb').read())  
-            try:
-                result = self.client_baidu_face.search(image, self.imageType, self.groupIdList);  # 调用人脸搜索在人脸库中搜索人脸返回json值
-                face_id = result['result']['user_list'][0]['user_id']   # 对JSON进行分析获取图片ID(学号)
-                # print(face_id)
-                time.sleep(0.3) # 同时调用次数太多导致api 故加300毫秒延迟  
+            if filename.endswith(".jpg"):
+                face_path = self.facefolder + filename;   
+                image = base64.b64encode(open(face_path, 'rb').read())  
+                try:
+                    result = self.client_baidu_face.search(image, self.imageType, self.groupIdList);  # 调用人脸搜索在人脸库中搜索人脸返回json值
+                    face_id = result['result']['user_list'][0]['user_id']   # 对JSON进行分析获取图片ID(学号)
+                    # print(face_id)
+                    time.sleep(0.3) # 同时调用次数太多导致api 故加300毫秒延迟  
 
-                faceDataBase = self.initdatabase()
-                student_id = str(face_id)
-                # 判断学生出勤情况
-                #self.get_headup_rate_each_student(student_id)
-                #print(str(student_id)+"的出勤情况已登记")
+                    faceDataBase = self.initdatabase()
+                    student_id = str(face_id)
+                    # 判断学生出勤情况
+                    #self.get_headup_rate_each_student(student_id)
+                    #print(str(student_id)+"的出勤情况已登记")
 
-                student_name = faceDataBase.get_student_name(student_id) # 返回学生姓名
-                if student_name is not None:
-                    student_name_list.append(student_name)
-                    print("识别成功，已存储")
+                    student_name = faceDataBase.get_student_name(student_id) # 返回学生姓名
+                    if student_name is not None:
+                        student_name_list.append(student_name)
+                        print("识别成功，已存储")
 
-            except Exception:
-                pass
+                except Exception:
+                    pass
         return student_name_list
 
 
