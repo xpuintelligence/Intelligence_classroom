71ff93b55abf700c0101dfdff39c6214c057b0b2
ahojcn <hanoi_ahoj@icloud.com>
2019-05-06 13:00:28 +0800

    使用动态库animate重新制作动画

--- ahoj-web-new/src/main.js
+++ ahoj-web-new/src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
+import './assets/animate.css'
 
 Vue.config.productionTip = false;
 
--- ahoj-web-new/src/router/index.js
+++ ahoj-web-new/src/router/index.js
@@ -2,13 +2,16 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
 import Login from '@/components/Login'
-import resource from 'vue-resource/dist/vue-resource.esm'
-import VueJsonp from 'vue-jsonp'
+import resource from 'vue-resource'
+// import VueJsonp from 'vue-jsonp'
 
 
 Vue.use(Router);
 Vue.use(resource);
-Vue.use(VueJsonp);
+// Vue.use(VueJsonp);
+
+Vue.http.options.root = 'http://47.103.14.73/';
+Vue.http.options.emulateJSON = true;
 
 export default new Router({
   routes: [
