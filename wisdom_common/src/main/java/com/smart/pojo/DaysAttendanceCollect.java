0e8f9f304bbf338506647d9c78fba9954f5c783c
LM123Code <1456466514@qq.com>
2019-05-16 20:49:04 +0800

    添加了平均抬头分数

--- wisdom_common/src/main/java/com/smart/pojo/DaysAttendanceCollect.java
+++ wisdom_common/src/main/java/com/smart/pojo/DaysAttendanceCollect.java
@@ -14,5 +14,6 @@ public class DaysAttendanceCollect implements Serializable {
     String date;//日期
     String sum;//总课程数
     String avg_head_up_rate;//平均抬头率
+    String avg_head_up_score;//平均抬头分数
     String avgGoal;//平均考勤分
 }
